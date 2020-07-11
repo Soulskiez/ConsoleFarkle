d9c38bc53b572589c55dcb7be117877dd541c2ed
Soulskiez <tylersolnikowski@gmail.com>
2020-07-11 13:06:57 -0400

    Initial

---
+++ Game.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FarkleCSharp
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Game start wooh");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FarkleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            x = Console.ReadLine();
+            Console.WriteLine("You typed: " + x);
+        }
+    }
+}
